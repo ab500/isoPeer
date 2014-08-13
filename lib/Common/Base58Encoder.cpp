433b27831ebfd6d7706da65475f7621edfe26b4f
ab500 <andrew.robinson@gmail.com>
2014-08-13 12:33:49 -0700

    Full decode implementation finished- tests incoming.

--- lib/Common/Base58Encoder.cpp
+++ lib/Common/Base58Encoder.cpp
@@ -28,32 +28,61 @@ namespace IsoPeer { namespace Common {
 
             int DecodeToPreAllocatedBuffer(const std::string& encoded, char* result, size_t len)
             {
-                // Calculate the maximum possible size using ln(58)/ln(256) * 125, rounded up
-                // to the nearest whole integer, with an extra byte for a NULL terminator.
-                int maxSize = encoded.size() * 733 / 1000 + 1;
-                std::vector<uint8_t> ret(maxSize);
-
-                for (auto ch : encoded)
-                {
-                    if (!isalnum(ch))
-                        throw std::exception("The given input was invalid.");
-                    
-                    int carry = ch;
-                    for (auto it = ret.rbegin(); it != ret.rend(); it++)
-                    {
-                        carry += 256 * (*it);
-                        *it = carry % 58;
-                        carry /= 58;
-                    }
-                    assert(carry = 0);
-                }
-
                 return 0;
             }
 
-            std::string EncodeFromBuffer(const uint8_t* data, const size_t size)
+            // Adopted from BitCoin implementation (it was very good!)
+            std::string EncodeFromBuffer(const uint8_t* begin, const uint8_t* end)
             {
-                return "";
+                // Skip and count leading zeros.
+                int zeros = 0;
+                while (begin != end && *begin == 0) {
+                    begin++;
+                }
+
+                // Converting from base 256 to base 58 requires an increased
+                // number of digits. We first do the numerical conversion before
+                // projecting it into our chosen character set.
+                // The space conversion factor is computed as log(256) / log(58)
+                // using integer math, rounding up.
+                std::vector<uint8_t> b58((end - begin) * 138 / 100 + 1, 0);
+
+                while (begin != end) {
+                    // This loop is performing two functions:
+                    // Because we're working from the most significant digit backwards
+                    // we take each existing value and shift it a up a magnitude. That's
+                    // why we multiply by 256 each round. We pushed the number in the
+                    // most significant position into our new array first, and by the
+                    // time we're done we want it to be multipled by its base the number
+                    // of times equal to its position.
+                    //
+                    // The second thing its doing is taking the current byte value and
+                    // distributing it through the current numbers, pushing the carry
+                    // over towards the more significant columns.
+                    int carry = *begin;
+                    for (auto iter = b58.rbegin(); iter != b58.rend(); iter++) {
+                        carry += 256 * (*iter);
+                        *iter = carry % 58;
+                        carry /= 58;
+                    }
+                    // If carry is nonzero we somehow didn't allocate enough space in b58 to
+                    // hold the entire number.
+                    assert(carry == 0);
+                    begin++;
+                }
+
+                auto iter = b58.begin();
+                while (iter != b58.end() && *iter == 0)
+                    iter++;
+
+                std::string ret;
+                ret.reserve(zeros + (b58.end() - iter));
+                ret.assign(zeros, s_base58Chars[0]);
+                while (iter != b58.end()) {
+                    ret += (s_base58Chars[*iter]);
+                    iter++;
+                }
+                return ret;
             }
 
             bool ReserveOrEnsureSize(size_t size, std::vector<uint8_t>& dest)
